b518a46800ad7b96af8d8aa9947c1a48f29238f3
luka52 <78267234+luka52@users.noreply.github.com>
2021-02-01 22:47:55 +0100

    Delete glowEffect.js

--- glowEffect.js
+++
@@ -1,9 +0,0 @@
-function glow() {var wrapper = document.querySelector(".glowEffect");
-wrapper.classList.add('hdnsn');
-  const linkccc = document.createElement('link')
-linkccc.type="text/css";
-linkccc.rel="stylesheet";
-linkccc.href="https://luka52.github.io/frameworks/cssforglowEffect.css";
-  document.head.appendChild(linkccc)
-}
-glow();
