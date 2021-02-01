dc212ded81d3510ca5e6ce9eb2e67484c487b27f
luka52 <78267234+luka52@users.noreply.github.com>
2021-02-01 22:38:19 +0100

    Add files via upload

---
+++ glowEffect.js
@@ -0,0 +1,8 @@
+function glow() {var wrapper = document.querySelector(".glowEffect");wrapper.classList.add('hdnsn');
+  const linkccc = document.createElement('link')
+linkccc.type="text/css";
+linkccc.rel="stylesheet";
+linkccc.href="cssforg.css";
+  document.head.appendChild(linkccc)
+}
+glow();
