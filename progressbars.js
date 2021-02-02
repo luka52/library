2e00a1cb76b3c2da28d7b279f8b3b1fa3d7a7c3f
luka52 <78267234+luka52@users.noreply.github.com>
2021-02-02 09:53:43 +0100

    Update progressbars.js

--- progressbars.js
+++ progressbars.js
@@ -1,11 +1,9 @@
 
-function ProgressBar(whggs,widthhh) {
-  let whatselest=document.querySelector(whggs+" span");
-  let progreslinee = document.querySelector("progress-line");
-  whatselest.style.width=widthhh;
+function ProgressBar() {
   const linkpppp=document.createElement('link')
 linkpppp.type="text/css";
 linkpppp.rel="stylesheet";
 linkpppp.href="progress.css";
 document.head.appendChild(linkpppp)
 }
+ProgressBar();
